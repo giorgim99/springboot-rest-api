7f5b612d26983cc9861010dc0f771adfc80ed1c7
Giorgi Maisuradze <maisuradze200329@gmail.com>
2023-12-12 10:47:51 +0400

    init-project

---
+++ src/test/java/ge/mais/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.mais.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
